58678d2af220304ca4be8a78e880b26655dced8d
Adrian Ardizza <tmgfaction@gmail.com>
2023-04-30 12:42:02 +0700

    Create inquiry payload documentation

--- web/cores/constants.py
+++ web/cores/constants.py
@@ -4,3 +4,14 @@ class SwaggerTag:
     INQUIRY = "Inquiry"
     VIRTUAL_ACCOUNT = "Virtual Account"
     AUTHENTICATION = "Authentication"
+
+class DecimalConstant:
+    MAX_DIGITS = 32
+    DECIMAL_PLACES = 4 
+
+TRANSACTION_TYPE_CHOICES = (
+    ("all", "All"),
+    ("top_up", "Top Up"),
+    ("transfer", "Transfer"),
+    ("virtual_account", "Virtual Account")
+)
--- web/transaction/views/inquiry.py
+++ web/transaction/views/inquiry.py
@@ -1,10 +1,11 @@
 from django.utils import timezone
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.renderers import serializers
+from rest_framework.serializers import ListSerializer
 from rest_framework.views import APIView, Response, status
 from rest_framework import permissions
 
-from cores.constants import SwaggerTag
+from cores.constants import TRANSACTION_TYPE_CHOICES, DecimalConstant, SwaggerTag
 
 
 class InquiryView(APIView):
@@ -13,17 +14,27 @@ class InquiryView(APIView):
     class InquiryRequestSerializer(serializers.Serializer):
         start_date = serializers.DateTimeField(allow_null=True, default=timezone.now() - timezone.timedelta(days=7))
         end_date = serializers.DateTimeField(allow_null=True, default=timezone.now())
-        filter_type = serializers.ChoiceField(choices=(
-            ("all", "All"),
-            ("top_up", "Top Up"),
-            ("transfer", "Transfer"),
-            ("virtual_account", "Virtual Account")
-        ))
+        filter_type = serializers.ChoiceField(
+            choices=TRANSACTION_TYPE_CHOICES
+        )
+
+    class InquiryResponseSerializer(serializers.Serializer):
+        class TransactionSerializer(serializers.Serializer):
+            id = serializers.UUIDField()
+            transaction_date = serializers.DateTimeField()
+            amount = serializers.DecimalField(
+                max_digits=DecimalConstant.MAX_DIGITS, 
+                decimal_places=DecimalConstant.DECIMAL_PLACES
+            )
+        
+        items = ListSerializer(
+            child=TransactionSerializer()
+        )
 
     @swagger_auto_schema(
         request_body=InquiryRequestSerializer(),
         responses={
-            "200": None 
+            "200": InquiryResponseSerializer(),
         },
         tags=[SwaggerTag.INQUIRY],
     )
