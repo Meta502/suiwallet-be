a6a9863828bf7951940615f53f42b2d22df263ab
stndaru <stefanus.ndaru@ui.ac.id>
2023-05-21 23:28:49 +0700

    Added ability to create customer

--- web/cores/serializers/authentication/registration.py
+++ web/cores/serializers/authentication/registration.py
@@ -4,6 +4,11 @@ class RegistrationRequestSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=128)
     password = serializers.CharField(max_length=128)
     email = serializers.CharField(max_length=128)
+    nik = serializers.CharField(max_length=16)
+    birth_date = serializers.DateTimeField()
+    phone_number = serializers.CharField(max_length=64)
+    address = serializers.CharField(max_length=1024)
+    postal_code = serializers.CharField(max_length=16)
 
 
 class RegistrationResponseSerializer(serializers.Serializer):
--- web/cores/services/register_customer.py
+++ web/cores/services/register_customer.py
@@ -1,22 +1,47 @@
 from dataclasses import dataclass
 from django.contrib.auth.models import User
+from customer.models import Customer, Address
 from rest_framework.authtoken.models import Token
+import datetime
 
 @dataclass
 class RegisterCustomerDataClass:
     username: str
     email: str
+    nik: str
+    birth_date: datetime.date
+    phone_number: str
+    address: str
+    postal_code: str
 
 class RegisterCustomerService:
     @classmethod
-    def run(cls, username: str, password: str, email: str) -> RegisterCustomerDataClass:
+    def run(cls, username: str, password: str, email: str, nik: str, birth_date: datetime.date, phone_number: str, address: str, postal_code: str) -> RegisterCustomerDataClass:
         user = User.objects.create_user(
             username,
             email,
             password,
         )
 
+        customer = Customer.objects.create(
+            user = user,
+            birth_date = birth_date,
+            phone_number = phone_number,
+            identification_number = nik
+        )
+
+        addressObj = Address.objects.create(
+            customer = customer,
+            address_text = address,
+            postal_code = postal_code
+        )
+
         return RegisterCustomerDataClass(
             username=user.username,
             email=user.email,
+            nik=customer.identification_number,
+            birth_date=customer.birth_date,
+            phone_number=customer.phone_number,
+            address=addressObj.address_text,
+            postal_code=addressObj.postal_code
         )
