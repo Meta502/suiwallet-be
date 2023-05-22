7f434a13d0980126f5d0637c251a95235847c872
Adrian Ardizza <tmgfaction@gmail.com>
2023-05-22 21:39:40 +0700

    Modify serializers

--- web/transaction/serializers/transfer.py
+++ web/transaction/serializers/transfer.py
@@ -3,13 +3,9 @@ from rest_framework import serializers
 from cores.constants import DecimalConstant
 
 class CreateTransferRequestSerializer(serializers.Serializer):
-    target_account_number=serializers.CharField()
+    target_account=serializers.CharField()
     amount=serializers.DecimalField(max_digits=DecimalConstant.MAX_DIGITS, decimal_places=DecimalConstant.DECIMAL_PLACES)
-    notes=serializers.CharField()
 
 class CreateTransferResponseSerializer(serializers.Serializer):
     transaction_id=serializers.CharField()
-    target_account_number=serializers.CharField()
     amount=serializers.DecimalField(max_digits=DecimalConstant.MAX_DIGITS, decimal_places=DecimalConstant.DECIMAL_PLACES)
-    notes=serializers.CharField()
-    message=serializers.CharField()
--- web/transaction/views/transfer.py
+++ web/transaction/views/transfer.py
@@ -1,10 +1,13 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions
-from rest_framework.authtoken.views import APIView
+from rest_framework.authtoken.views import APIView, Response
+from rest_framework.compat import requests
 
 from cores.constants import SwaggerTag
 from transaction.serializers.transfer import CreateTransferRequestSerializer, CreateTransferResponseSerializer
 
+from os import environ
+TRANSACTION_SERVICE_URL = environ.get("TRANSACTION_SERVICE_HOST")
 
 class TransferView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -17,4 +20,10 @@ class TransferView(APIView):
         tags=[SwaggerTag.TRANSFER]
     )
     def post(self, request):
-        pass
+        api_request = requests.post(f"{TRANSACTION_SERVICE_URL}/transfer", json={
+            "sourceId": str(request.user.id),
+            "targetId": request.data["target_account"],
+            "amount": request.data["amount"],
+        })
+
+        return Response(api_request.json(), status=api_request.status_code)
